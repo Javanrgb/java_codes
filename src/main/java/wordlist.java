908836d8e846d57fadac8b910a7d389c05d5260a
Javanrgb <85636992+Javanrgb@users.noreply.github.com>
2022-02-23 05:03:57 +0300

    random_wordlist

--- src/main/java/wordlist.java
+++ src/main/java/wordlist.java
@@ -1,19 +1,24 @@
-//import math.*;
-public class wordlist {
+
+public class wordlist {//class definition
     public static void main(String[] args) {
-        String wordlist1[] = {"Hello","Boy","24/7","Encyclopaedia","man"};
+        String wordlist1[] = {"Hello","Boy","24/7","Encyclopaedia","man"};//Creating a list of words
         String wordlist2 [] = {"came","talked","sang"};
         String wordlist3[] = {"here","now","quickly","loudly"};
-        int onelength= wordlist1.length; //determining length of each wordlist
-        int twolength= wordlist2.length;
+        
+        //determining length of each wordlist
+        int onelength= wordlist1.length; 
+        int twolength= wordlist2.length;//
         int threelength= wordlist3.length;
+        
+        
 
+        //creating a set of random words from our created lists
         int rand1 =  (int) (Math.random()*onelength);
         int rand2 =  (int) (Math.random()*onelength);
         int rand3 =  (int) (Math.random()*onelength);
 
-        String phrase = wordlist1[rand1] +" " +wordlist2[rand2] +" "+wordlist3[rand3];
-        System.out.println("Sample statement " +phrase);
+        String phrase = wordlist1[rand1] +" " +wordlist2[rand2] +" "+wordlist3[rand3];//creating a phrase out of the random words in each list
+        System.out.println("Sample statement " +phrase);// printing to the standard output a sample output of a phrase
 
     }
 }
